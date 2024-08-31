6aa62b3b5c12bc74ffaef869546e6a92084b3234
abhiram-nss-07 <abhiramnss07@gmail.com>
2024-08-31 09:54:15 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+USER_NAME = "Abhiram"
+
+TARGET_DIRECTORY = os.path.expanduser("~/Downloads")
+
+REMINDER_FILE = os.path.expanduser("~/reminders.txt")
+
+WEATHER_API_URL = "https://wttr.in"
+LOCATION = "Delhi"  
+
+NOTIFICATION_TITLE = "Personal Assistant Notification"
